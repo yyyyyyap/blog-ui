a5992b160ce9e3728ab904fd1f9c82187fbb339f
Yap <2010705117@qq.com>
2021-08-13 22:19:43 +0800

    博客框架基本构建

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './pages/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-main>
+      </el-main>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      article: {
+        title: '',
+        author: '',
+        content: '',
+        category: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: gold;
+  }
+  .el-main {
+    height: 500px;
+    background-color: goldenrod;
+  }
+  .el-footer {
+    background-color: greenyellow;
+  }
+</style>
